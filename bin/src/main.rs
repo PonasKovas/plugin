63ef0c948273ca11a2157a436e8dbe2f138ad0ad
PonasKovas <mykolas.peteraitis@gmail.com>
2021-07-19 11:04:04 +0300

    async

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -1,11 +1,15 @@
+use async_ffi::FfiFuture;
 use tokio::runtime::{Handle, Runtime};
 
 fn main() {
     let runtime = Runtime::new().unwrap();
-    unsafe {
-        let lib = libloading::Library::new("../plugin/target/release/libplugin.so").unwrap();
-        let func: libloading::Symbol<extern "C" fn(&Handle)> = lib.get(b"something").unwrap();
-        func(runtime.handle());
-    }
-    std::thread::park();
+    let handle = runtime.handle();
+    runtime.block_on(async move {
+        unsafe {
+            let lib = libloading::Library::new("../plugin/target/release/libplugin.so").unwrap();
+            let func: libloading::Symbol<extern "C" fn(&Handle) -> FfiFuture<()>> =
+                lib.get(b"something").unwrap();
+            func(handle).await;
+        }
+    });
 }
--- plugin/src/lib.rs
+++ plugin/src/lib.rs
@@ -1,10 +1,13 @@
+use async_ffi::{FfiFuture, FutureExt};
 use tokio::runtime::Handle;
 
 #[no_mangle]
-extern "C" fn something(runtime: &Handle) {
-    runtime.block_on(async move {
+extern "C" fn something(runtime: Handle) -> FfiFuture<()> {
+    async move {
+        let _guard = runtime.enter();
         println!("hello");
         tokio::time::sleep(std::time::Duration::from_secs(5)).await;
-        println!("yeah");
-    });
+        println!("yeah from plugin");
+    }
+    .into_ffi()
 }
